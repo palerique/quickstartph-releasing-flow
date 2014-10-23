aa58c7bf0a92e4ec5bc7709ea1486b2430aba74f
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2014-10-23 19:10:53 -0200

    ADD: data no olá mundo, ADD: plugin do git-flow ao pom.xml

--- src/main/java/ph/sitedo/quickstartph/App.java
+++ src/main/java/ph/sitedo/quickstartph/App.java
@@ -1,5 +1,7 @@
 package ph.sitedo.quickstartph;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World! Now is " + new Date() );
     }
 }
